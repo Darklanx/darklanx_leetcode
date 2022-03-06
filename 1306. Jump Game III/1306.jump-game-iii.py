2078287e23cc22fc9e2f3bdce6443cf553e93039
csdarklanx <csdarklanx@gmail.com>
2022-03-06 23:06:55 +0800

    1306 jump game iii

---
+++ 1306. Jump Game III/1306.jump-game-iii.py
@@ -0,0 +1,23 @@
+from typing import List
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if start > len(arr) or start < 0:
+            raise ValueError(f"Invalid starting position: {start}")
+        
+        def valid(index):
+            return index >= 0 and index < len(arr)
+        def explore(pos: int) -> bool:
+            if pos in visited:
+                return False
+            visited.add(pos)
+            if arr[pos] == 0:
+                return True
+            
+            for next_pos in [pos + arr[pos], pos - arr[pos]]:
+                if valid(next_pos):
+                    if explore(next_pos):
+                        return True
+
+        visited = set()
+
+        return explore(start)
---
+++ 1306. Jump Game III/explanation.md
@@ -0,0 +1,3 @@
+[1306. Jump Game III](https://leetcode.com/problems/jump-game-iii/)
+===
+Simple DFS, time complexity is $O(n)$ since you can only visit a position once, and there are only `n` positions.
