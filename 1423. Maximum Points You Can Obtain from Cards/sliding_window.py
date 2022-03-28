47a665bc033739d75e12665ac1db226348079587
csdarklanx <csdarklanx@gmail.com>
2022-03-28 23:22:39 +0800

    1423

--- 1423. Maximum Points You Can Obtain from Cards/sliding_window.py
+++ 1423. Maximum Points You Can Obtain from Cards/sliding_window.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-    max_score = cur_score = sum(cardPoints[:k])
-    for i in range(1, k + 1):
-        cur_score += cardPoints[-i] - cardPoints[k-i]  # right_side - left_side
-        max_score = max(cur_score, max_score)
-    return max_score
+        max_score = cur_score = sum(cardPoints[:k])
+        for i in range(1, k + 1):
+            cur_score += cardPoints[-i] - cardPoints[k-i]  # right_side - left_side
+            max_score = max(cur_score, max_score)
+        return max_score
